877a13ceb1063f1cfdd044fe3ed7bd556b489e55
NickyCV-369 <nickycv2004@gmail.com>
2025-07-20 16:56:26 +0700

    Initial commit - Microservice E-commerce with Docker

--- payment-service/initDB.js
+++ payment-service/initDB.js
@@ -6,7 +6,7 @@ const initDB = async () => {
       id SERIAL PRIMARY KEY,
       order_id VARCHAR(50) NOT NULL,
       amount NUMERIC NOT NULL,
-      status VARCHAR(20) NOT NULL,
+      status VARCHAR(50) NOT NULL,
       payment_intent_id VARCHAR(100),
       created_at TIMESTAMP DEFAULT NOW()
     )
